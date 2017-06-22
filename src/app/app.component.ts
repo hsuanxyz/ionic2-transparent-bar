54a52e344f0b07d413c336664af7327e9fd32c4d
HsuanXyz <HsuanGM@gmail.com>
2017-06-22 23:27:26 +0800

    Update Ionic Framework  to 3.4.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -15,8 +14,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
+
     });
   }
 }
